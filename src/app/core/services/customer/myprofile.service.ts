bb937fac5ce99806d0acad3ff917bd9928bc6307
Shubam Mondal <mondalshubham14@gmail.com>
2021-09-26 13:33:10 +0530

    get api change

--- src/app/core/services/customer/myprofile.service.ts
+++ src/app/core/services/customer/myprofile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { MyProfileReqModel } from '@core/models/customer';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ApiService } from '../api.service';
 
 @Injectable({
@@ -11,6 +11,6 @@ export class MyprofileService {
   constructor(private _apiService: ApiService) { }
 
   getMyProfile(): Observable<any> {
-    return this._apiService.MyProfile.create(null, null, null).map(response => response)
+    return this._apiService.MyProfile.getAll(null, null).pipe(map(response => response));
   }
 }
--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,7 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { MyProfileReqModel } from '@core/models/customer';
+// import { MyProfileReqModel } from '@core/models/customer';
 import { MyprofileService } from '@core/services/customer/myprofile.service';
 import { UserLoginService } from '@core/services/user-login.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-profile',
@@ -17,16 +19,27 @@ export class MyProfileComponent implements OnInit {
   }
 
   getProfileData(): void {
-    this.myprofileService.getMyProfile().subscribe(res => {
-      console.log('res ', res);
-    });
+    // this.myprofileService.getMyProfile().subscribe(res => {
+    //   console.log('res ', res);
+    // });
+
+    this.myprofileService.getMyProfile()
+      .pipe(finalize(() => {
+        // tslint:disable-next-line: deprecation
+      })).subscribe(response => {
+        console.log(response);
+      }, error => {
+        if (error instanceof HttpErrorResponse) {
+         console.log(error);
+        }
+      });
   }
 
   createResModel() {
     this.userLoginService.decodeJwt();
-    let myProfileReqModel = new MyProfileReqModel();
-    myProfileReqModel.id = this.userLoginService.jwtTokenValue.nameid;
-    return myProfileReqModel;
+    // let myProfileReqModel = new MyProfileReqModel();
+    // myProfileReqModel.id = this.userLoginService.jwtTokenValue.nameid;
+    // return myProfileReqModel;
   }
 
 
